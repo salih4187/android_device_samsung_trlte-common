38797981e346e40fe8d2525d90a2fe2036d6c5c0
Salih Çağlar <salihcaglar4187@gmail.com>
2025-08-17 19:13:08 +0300

    trlte-common: Move supplicant service init to wpa_supplicant

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,3 +196,4 @@ WIFI_DRIVER_FW_PATH_PARAM   := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA     := "/vendor/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP      := "/vendor/etc/wifi/bcmdhd_apsta.bin"
 WIFI_BUS := PCIE
+WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
