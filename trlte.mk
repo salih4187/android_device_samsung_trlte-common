ed23bd27d57781d24a95ad828ef9d46ffdfb0318
Salih Çağlar <salihcaglar4187@gmail.com>
2025-09-18 20:38:54 +0300

    trlte-common: Use legacy HAL1 camera HIDL Also remove extra spaces

--- trlte.mk
+++ trlte.mk
@@ -82,7 +82,7 @@ PRODUCT_PACKAGES += \
 # APEX
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/swcodec/ld.config.txt
-    
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
@@ -99,8 +99,8 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.apq8084 \
     libxml2 \
     Snap
